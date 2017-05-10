ce61acd649cca78d99425e737f51955fabb8193d
Daniel Medeiros <danielmmf@gmail.com>
2017-05-10 13:58:26 -0300

    passando no primero teste de grupo

--- tests/unit/AppGroupTest.php
+++ tests/unit/AppGroupTest.php
@@ -17,10 +17,32 @@ class AppGroupTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testMe()
+    public function testDeveSaberAQuantidadeDeGruposCriados()
     {
+
         $todos =  App\Group::all();
         $this->assertEquals(0 , $todos->count());
-     
+
+        $grupo =  new App\Group;
+        $grupo->name = "grupo de teste";
+        $grupo->save();
+      //  print_r($grupo);
+
+        $todos =  App\Group::all();
+        //print_r($todos);//descomentar se precisar ver o log mais detalhado
+        //die("//***************aqui em cima é log // ");
+
+        $this->assertEquals(1 , $todos->count());
+
+        $grupo =  new App\Group;
+        $grupo->name = "grupo de teste 1";
+        $grupo->save();
+
+        $todos =  App\Group::all();
+        //print_r($todos);//descomentar se precisar ver o log mais detalhado
+        //die("//***************aqui em cima é log // ");
+
+        $this->assertEquals(2 , $todos->count());
+
     }
 }
